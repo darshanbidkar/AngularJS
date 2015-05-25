485446ecf4fac6169836c75fa7d9cdeacb1356a5
Darshan Bidkar <darshan.bidkar@gmail.com>
2015-05-25 03:43:04 -0500

    http get try

--- Client/home.js
+++ Client/home.js
@@ -1,7 +1,12 @@
  var home = angular.module("homeApp", []);
             home.controller("homeController",
-            function($scope) {
+            function($scope, $http) {
               $scope.authenticate = function() {
                 $scope.message = 'done';
+                var dropboxurl = "https://www.dropbox.com/1/oauth2/authorize";
+                var headers = {headers: {'response_type': 'token', 'client_id':'hw8rgsgolaui1qc'}};
+                $http.get(dropboxurl, headers).success(function(response) {
+                    $scope.message='yes';
+                });
               }
             });
