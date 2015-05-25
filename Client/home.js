5e4a80a38edf4aa67df666833367949b9bbd0c9e
Darshan Bidkar <darshan.bidkar@gmail.com>
2015-05-25 14:02:10 -0500

    dropbox redirect done

--- Client/home.js
+++ Client/home.js
@@ -4,11 +4,7 @@
               $scope.authenticate = function() {
                 $scope.message = 'done';
                 var dropboxurl = "https://www.dropbox.com/1/oauth2/authorize";
-                  $window.location.href=dropboxurl;
-                $location.url($window.location.href);
-                  $location.search({
-                    'response_type': 'code', 
-                   'client_id':'hw8rgsgolaui1qc'
-                  });
+                  var authUrl = dropboxurl+"?response_type=code&client_id=hw8rgsgolaui1qc";
+                  $window.location.href=authUrl;
               }
             });
