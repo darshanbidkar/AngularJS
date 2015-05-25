01bac169260559841e40b842cd085e454d130257
Darshan Bidkar <darshan.bidkar@gmail.com>
2015-05-25 14:16:01 -0500

    better.. using dropbox.js lib

--- Client/home.js
+++ Client/home.js
@@ -1,10 +1,6 @@
- var home = angular.module("homeApp", []);
-            home.controller("homeController",
-            function($scope, $location, $window) {
-              $scope.authenticate = function() {
-                $scope.message = 'done';
-                var dropboxurl = "https://www.dropbox.com/1/oauth2/authorize";
-                  var authUrl = dropboxurl+"?response_type=code&client_id=hw8rgsgolaui1qc";
-                  $window.location.href=authUrl;
-              }
-            });
+var home = angular.module("homeApp", []);
+home.controller("homeController", function($scope) {
+    $scope.authenticate = function() {
+        var client = new Dropbox.Client({key: "hw8rgsgolaui1qc"});
+    }
+});
